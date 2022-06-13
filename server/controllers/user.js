7bc815c8135335bde6acaf7d5c342d9cff6775ac
Kemal <adzhimambetov_ks@nlmk.com>
2022-06-13 15:37:45 +0300

    add auth and reg

--- server/controllers/user.js
+++ server/controllers/user.js
@@ -1,13 +1,61 @@
-class User {
-  async registration(req, res) {
-    res.json("reg");
+const ApiError = require("../error/ApiError");
+const bcrypt = require("bcrypt");
+const { User, Basket } = require("../models/models");
+const jswt = require("jsonwebtoken");
+
+const generateJwt = (id, email, role) => {
+  return jswt.sign({ id, email, role }, process.env.SECRET_KEY, {
+    expiresIn: "24h",
+  });
+};
+
+class UserController {
+  async registration(req, res, next) {
+    const { email, password, role } = req.body;
+    if (!email || !password) {
+      return next(ApiError.badRequest("Некорректный email или password"));
+    }
+
+    const candiate = await User.findOne({ where: { email } });
+
+    if (candiate) {
+      return next(ApiError.badRequest("Пользователь с таким email существует"));
+    }
+
+    const hashPswd = await bcrypt.hash(password.toString(), 5);
+    const user = await User.create({ email, role, password: hashPswd });
+    const basket = await Basket.create({ userId: user.id });
+    const token = generateJwt(user.id, email, user.role);
+
+    return res.json({ token });
   }
 
-  async login(req, res) {}
+  async login(req, res, next) {
+    const { email, password } = req.body;
+
+    const user = await User.findOne({ where: { email } });
+
+    if (!user) {
+      next(ApiError.badRequest("Пользователь с таким email не найден"));
+    }
+
+    console.log("pswd", password);
+
+    let comparePswd = bcrypt.compareSync(password, user.password);
+
+    if (!comparePswd) {
+      next(ApiError.badRequest("Указанный пароль неверный"));
+    }
+
+    const token = generateJwt(user.id, user.email, user.role);
+
+    return res.json({ token });
+  }
 
   async check(req, res) {
-    res.json("asfsdf");
+    const token = generateJwt(req.user.id, req.user.email, req.user.role);
+    return res.json({ token });
   }
 }
 
-module.exports = new User();
+module.exports = new UserController();
